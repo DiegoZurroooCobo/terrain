2636505357259e19d7d507925d5d8819d22699e7
DiegoZurroooCobo <dizuco25@gmail.com>
2024-10-17 10:41:59 +0200

    varias acciones

--- terrain_construction/Assets/Scripts/IA/State.cs
+++ terrain_construction/Assets/Scripts/IA/State.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 public struct StateParameters
 {
     [Tooltip("Indicates if the actions check must be true or false")]
-    public bool actionValue;
+    public bool[] actionsValues;
     [Tooltip("Action that is gonna be executed")]
-    public Action action;
+    public Action[] actions;
     [Tooltip("if the actions check equals actionsValue, nextState is pushed")]
     public State nextState;
+    [Tooltip("All actions are executed or just one?")]
+    public bool and;
 }
 public abstract class State : ScriptableObject
 {
@@ -20,9 +22,12 @@ public abstract class State : ScriptableObject
 
     public void DrawAllActionsGizmos(GameObject owner) 
     { 
-        for(int i = 0; i < stateparameters.Length; i++) 
+        foreach( StateParameters parameters in stateparameters ) 
         {
-            stateparameters[i].action.DrawGizmos(owner);
+            foreach(Action actions in parameters.actions) 
+            { 
+                actions.DrawGizmos(owner);
+            }
         }
     }
 
@@ -33,9 +38,26 @@ public abstract class State : ScriptableObject
     { 
         for (int i = 0; i < stateparameters.Length; i++) // recorre el array de los parametros del estado
         {
-            if (stateparameters[i].actionValue == stateparameters[i].action.Check(owner))  // en cada vuelta, si el valor de la accion es igual a la accion que se va a realizar, se pasa a la siguiente accion
-            { 
-                return stateparameters[i].nextState; // devuelve la siguiente accion
+            bool todaslasAccionesSeHanCumplido = true;
+            for( int j = 0; j < stateparameters[i].actions.Length; j++) 
+            {
+                if (stateparameters[i].actions[j].Check(owner) == stateparameters[i].actionsValues[j]) 
+                {
+                    if (!stateparameters[i].and) // si solo se tiene que cumplir una 
+                    { 
+                        return stateparameters[i].nextState; // devuelve el siguiente estado 
+                    }
+                }
+                else if (stateparameters[i].and) // estanmos seguros de que esta accion no se ha cumplido. Se ha marcado que se deben cumplir todas
+                {
+                    todaslasAccionesSeHanCumplido = false;
+                    break; // salimos del bucle, porque una accion no se ha cumplido y estamos en and 
+                }
+            }
+            // si llegamos hasta aqui, significa que el disegnador que todas las acciones tienen que cumplirse. Ademas, se ha cumplido
+            if (stateparameters[i].and && todaslasAccionesSeHanCumplido) 
+            {
+                return stateparameters[i].nextState;
             }
         }
         return null;   // ninguna accion se cumple, por lo que no cambiamos de estado
