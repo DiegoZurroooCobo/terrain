b74ca1f584739fc44728f3d12b74967b3e35e2a5
DiegoZC <dizuco25@gmail.com>
2024-11-06 20:45:49 +0100

    arreglitos

--- terrain_construction/Assets/Scripts/Enemy damage.cs
+++ terrain_construction/Assets/Scripts/Enemy damage.cs
@@ -6,7 +6,7 @@ public class Enemydamage : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.GetComponent<PlayerMovement>() != null) 
+        if(collision.gameObject.GetComponent<PlayerMovement>()) 
         { 
             GameManager.instance.SetLifes(GameManager.instance.GetLifes() - 1);
         }
