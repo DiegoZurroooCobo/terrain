be42d65e63dd81bfdaefcfe8780b411786f53141
DiegoZC <dizuco25@gmail.com>
2024-11-13 20:33:13 +0100

    Audio Manager / Las IAs me chulean ayudaaaaaaa

--- terrain_construction/Assets/Scripts/IA/DamageState.cs
+++ terrain_construction/Assets/Scripts/IA/DamageState.cs
@@ -7,11 +7,17 @@ using UnityEngine;
 
 public class DamageState : State
 {
+    private float currentTime = 0f, maxTime;
     public override State Run(GameObject owner)
     {
         State nextstate = CheckActions(owner);
-
-        GameManager.instance.SetLifes(GameManager.instance.GetLifes() - 1); 
+        currentTime += Time.deltaTime;
+        if (currentTime >= maxTime)
+        {
+            GameManager.instance.SetLifes(GameManager.instance.GetLifes() - 1);
+            currentTime = 0f;
+            maxTime = 3f;
+        }
 
         return nextstate;
     }
